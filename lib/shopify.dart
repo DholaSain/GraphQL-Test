3bb3646b280e6328aa923fe0a44b9a662ad43a2c
Mujahid <71888363+DholaSain@users.noreply.github.com>
2021-09-21 00:13:11 +0500

    updated

--- lib/shopify.dart
+++ lib/shopify.dart
@@ -2,8 +2,8 @@ import 'package:flutter/material.dart';
 import 'package:graphqltest/Modal/DataModel.dart';
 
 class Shopify extends StatefulWidget {
-  List<Model> pv;
-  Shopify(this.pv);
+  List<Model> collection;
+  Shopify(this.collection);
 
   @override
   _ShopifyState createState() => _ShopifyState();
@@ -12,6 +12,7 @@ class Shopify extends StatefulWidget {
 class _ShopifyState extends State<Shopify> {
   @override
   Widget build(BuildContext context) {
+    // debugPrint('${widget.collection.length.toString()}');
     return SafeArea(
         child: Scaffold(
       appBar: AppBar(
@@ -19,127 +20,95 @@ class _ShopifyState extends State<Shopify> {
         title: Text('All products'),
       ),
       body: SingleChildScrollView(
-        physics: ScrollPhysics(),
-        child: Container(
-          child: ListView.builder(
-            physics: NeverScrollableScrollPhysics(),
-            shrinkWrap: true,
-            itemCount: widget.pv.length,
-            itemBuilder: (BuildContext context, int index) {
-              return Center(
-                  child: Container(
-                child: Column(
-                  children: [
-                    // Text(widget.pv[index].title.toString()),
-                    ListView.builder(
-                        physics: NeverScrollableScrollPhysics(),
-                        shrinkWrap: true,
-                        itemCount: widget.pv[index].products!.edges!.length,
-                        itemBuilder: (context, index) {
-                          var allProducts = widget.pv[index].products!.edges!;
-                          return Column(
-                            children: [
-                              GridView.builder(
-                                  physics: NeverScrollableScrollPhysics(),
-                                  shrinkWrap: true,
-                                  itemCount: allProducts[index]
-                                      .node!
-                                      .images!
-                                      .edges!
-                                      .length,
-                                  gridDelegate:
-                                      SliverGridDelegateWithFixedCrossAxisCount(
-                                          crossAxisCount: 2),
-                                  itemBuilder:
-                                      (BuildContext context, int index) {
-                                    var allImages =
-                                        allProducts[index].node!.images!.edges!;
-                                    return Container(
-                                      height: 200,
-                                      width: 200,
-                                      child: Image.network(allImages[index]
-                                          .node!
-                                          .originalSrc
-                                          .toString()),
-                                    );
-                                  }),
-                              // ListView.builder(
-                              //   physics: NeverScrollableScrollPhysics(),
-                              //   shrinkWrap: true,
-                              //   itemCount: allProducts[index]
-                              //       .node!
-                              //       .images!
-                              //       .edges!
-                              //       .length,
-                              //   itemBuilder: (context, int index) {
-                              //     var allImages =
-                              //         allProducts[index].node!.images!.edges!;
-                              //     return Container(
-                              //       height: 200,
-                              //       width: 200,
-                              //       child: Image.network(allImages[index]
-                              //           .node!
-                              //           .originalSrc
-                              //           .toString()),
-                              //     );
-                              //     // return ListView.builder(
-                              //     //     physics: NeverScrollableScrollPhysics(),
-                              //     //     shrinkWrap: true,
-                              //     //     itemCount: allImages[index].node.originalSrc.,
-                              //     //     itemBuilder: (context, int index) {
-                              //     //       return Container(
-                              //     //         width: 40,
-                              //     //         height: 100,
-                              //     //         child: Image.network(widget
-                              //     //             .pv[index]
-                              //     //             .products!
-                              //     //             .edges![index]
-                              //     //             .node!
-                              //     //             .images!
-                              //     //             .edges!
-                              //     //             .first
-                              //     //             .node!
-                              //     //             .originalSrc!
-                              //     //             .toString()),
-                              //     //       );
-                              //     //     });
-                              //   },
-                              // ),
-                              Text(widget
-                                  .pv[index].products!.edges![index].node!.title
-                                  .toString()),
-                            ],
-                          );
-                        }),
-                    // Container(
-                    //   width: 200,
-                    //   height: 200,
-                    //   child: Image.network(widget
-                    //       .pv[index]
-                    //       .products!
-                    //       .edges![index]
-                    //       .node!
-                    //       .images!
-                    //       .edges![index]
-                    //       .node!
-                    //       .originalSrc
-                    //       .toString()),
-                    // ),
+        physics: BouncingScrollPhysics(),
+        child: ListView.builder(
+          physics: NeverScrollableScrollPhysics(),
+          shrinkWrap: true,
+          itemCount: widget.collection.length,
+          itemBuilder: (BuildContext context, int index) {
+            var allCollections = widget.collection[index].products!.edges;
+            var collectionName = widget.collection[index].title;
+            // debugPrint(
+            //     '${widget.collection[index].products!.edges!.length.toString()}');
+            return Center(
+                child: Container(
+              child: Column(
+                children: [
+                  Container(
+                    height: 40,
+                    width: double.infinity,
+                    color: Colors.purple,
+                    alignment: Alignment.center,
+                    child: Text(
+                      collectionName!,
+                      style: TextStyle(color: Colors.white),
+                    ),
+                  ),
+                  ListView.builder(
+                      physics: NeverScrollableScrollPhysics(),
+                      shrinkWrap: true,
+                      itemCount: allCollections!.length,
+                      itemBuilder: (context, index) {
+                        var allProducts = allCollections[index].node;
+                        return Column(
+                          children: [
+                            Container(
+                              height: 40,
+                              width: double.infinity,
+                              color: Colors.blue,
+                            ),
+                            GridView.builder(
+                                physics: NeverScrollableScrollPhysics(),
+                                shrinkWrap: true,
+                                itemCount: allProducts!.images!.edges!.length,
+                                gridDelegate:
+                                    SliverGridDelegateWithFixedCrossAxisCount(
+                                        crossAxisCount: 2),
+                                itemBuilder: (BuildContext context, int index) {
+                                  // debugPrint(
+                                  //     '${allProducts[index].node!.images!.edges!.length.toString()}');
+                                  var allImages = allProducts.images!.edges!;
+                                  return Container(
+                                    height: 200,
+                                    width: 200,
+                                    child: Image.network(allImages[index]
+                                        .node!
+                                        .originalSrc
+                                        .toString()),
+                                  );
+                                }),
+                            Text(allProducts.title.toString()),
+                          ],
+                        );
+                      }),
+                  // Container(
+                  //   width: 200,
+                  //   height: 200,
+                  //   child: Image.network(widget
+                  //       .pv[index]
+                  //       .products!
+                  //       .edges![index]
+                  //       .node!
+                  //       .images!
+                  //       .edges![index]
+                  //       .node!
+                  //       .originalSrc
+                  //       .toString()),
+                  // ),
 
-                    // Text(
-                    //   widget.pv[index].title.toString(),
-                    //   style:
-                    //       TextStyle(fontSize: 16, fontWeight: FontWeight.bold),
-                    // ),
-                    // Container(
-                    //   width: 200,
-                    //   child: Text(widget.pv[index].products.),
-                    // ),
-                  ],
-                ),
-              ));
-            },
-          ),
+                  // Text(
+                  //   widget.pv[index].title.toString(),
+                  //   style:
+                  //       TextStyle(fontSize: 16, fontWeight: FontWeight.bold),
+                  // ),
+                  // Container(
+                  //   width: 200,
+                  //   child: Text(widget.pv[index].products.),
+                  // ),
+                ],
+              ),
+            ));
+          },
         ),
       ),
     ));
